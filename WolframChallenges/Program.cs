0c6ea7a917f01e4a6888c9fe5861c7434f270f4d
Travis Arsenault <t.arsenault@intrideo.com>
2019-11-03 15:36:04 -0500

    Added Riffling

--- WolframChallenges/Program.cs
+++ WolframChallenges/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WolframChallenges.Shared;
 
 namespace WolframChallenges
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DependencyInjection.BuildDependencyInjectionContainer();
+
+            List<List<object>> objectList = new List<List<object>>
+            {
+                new List<object>
+                {
+                    "a","b","c"
+                },
+                new List<object>
+                {
+                    1,2,3
+                }
+            };
+
+            DeriffleList deriffleList = DependencyInjection.Container.Resolve<DeriffleList>();
+            IUtilities utilities = DependencyInjection.Container.Resolve<IUtilities>();
+            string listString = utilities.GetListString(deriffleList.DeRiffle(deriffleList.Riffle(objectList), 2));
+
+
+            Console.WriteLine(listString);
+            //Pause to read output
+            Console.ReadLine();
         }
     }
 }
